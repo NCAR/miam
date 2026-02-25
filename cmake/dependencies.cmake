fcd33354d9e0980ef333d6ad8659de77937b30ad
Matt Dawson <matt@cohere-llc.com>
2026-02-25 10:41:35 -0800

    update MICM commit

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -24,7 +24,7 @@ endif()
 
 FetchContent_Declare(micm
     GIT_REPOSITORY https://github.com/NCAR/micm.git
-    GIT_TAG 582be1f0ce6756bc028f40a594edd6be68be390d
+    GIT_TAG 7d0af1d58a57fcbae35736ec417a095613c50450
     GIT_PROGRESS NOT ${FETCHCONTENT_QUIET}
     FIND_PACKAGE_ARGS NAMES micm
 )
