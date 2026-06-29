7e35e85d00b3e91cacfadc29ce79824e6a34cf0f
Jiwon Gim <jiwongim@ucar.edu>
2026-06-29 15:24:44 -0600

    bump google test version

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 if(MIAM_ENABLE_TESTS)
   FetchContent_Declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.16.0
+    GIT_TAG v1.17.0
   )
 
   set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
